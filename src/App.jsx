67b9ddc11a3bd6a884085c2038d93b3f29567e9f
Ubuntu <ahmedjibril681@gmail.com>
2024-05-30 18:08:33 -0400

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Navbar from './Component/Navbar/Navbar';
-import { Sidebar } from 'phosphor-react';
 import Admin from './Pages/Admin/Admin';
 
 const App = () => {
